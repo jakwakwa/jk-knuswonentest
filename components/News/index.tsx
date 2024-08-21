468341248e8f3dc7826b9506302ed84ef47c25a8
Jaco Kotzee <jakwakwa@gmail.com>
2024-08-21 14:16:44 +0200

    chore: Update News component layout and styles

--- components/News/index.tsx
+++ components/News/index.tsx
@@ -36,8 +36,8 @@ function News() {
     },
   ];
   return (
-    <section className="w-screen h-auto md:h-[572px] relative  bg-[#99a3d8] px-12 lg:px-64 xl:px-128 pt-0 pb-12 md:py-12">
-      <div className="flex flex-col gap-4 w-full lg:max-w-screen-lg h-auto md:h-[444px] mx-auto">
+    <section className="w-screen h-auto md:h-[572px] relative  bg-[#99a3d8] px-12 lg:px-32 xl:px-64 pt-0 pb-12 lg:py-12">
+      <div className="flex flex-col gap-4 w-full xl:max-w-screen-lg h-auto md:h-[444px] mx-auto">
         <div
           className={`w-3/4 px-12 pt-6 md:px-0 md:pt-0 md:w-[597px] text-[#39468c] text-[32px] font-extrabold ${overlock.className}
         `}
